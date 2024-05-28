f1d6a1ed181e7bcbb819c820de220a608c575dd8
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-05-28 01:42:56 -0600

    refactor: Remove unused code from SearchHome component

--- components/SearchHome.jsx
+++ components/SearchHome.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import ResponseSearch from "@/components/ResponseSearch";
@@ -42,21 +41,3 @@ export default function Component() {
   )
 }
 
-function MountainIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
-  )
-}
