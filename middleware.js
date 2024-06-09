d65a94f6bda59419dea797589156ac59e39786a5
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-09 17:52:30 -0600

    refactor: Update middleware to handle authentication and authorization

--- middleware.js
+++ middleware.js
@@ -1,31 +1,24 @@
 import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
 // Esta función puede ser marcada como `async` si usas `await` dentro
-export function middleware(request) {
-  return NextResponse.redirect(new URL('/', request.url));
+export async function middleware(request) {
+  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+  
+  // Si no hay token y la ruta es privada, redirigir a la página de inicio de sesión
+  if (!token && request.nextUrl.pathname.startsWith('/private')) {
+    const url = new URL('/auth/signin', request.url);
+    url.searchParams.set('callbackUrl', encodeURIComponent(request.url));
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
 }
 
 // Configuración del matcher
 export const config = {
-  matcher: ["/dashboard", "/private"],
+  matcher: [
+    '/dashboard/:path*',
+    '/private/:path*'
+  ],
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export { default } from "next-auth/middleware"
-
-// export const config = {
-//   matcher: ["/dashboard", "/private"]
-// }
