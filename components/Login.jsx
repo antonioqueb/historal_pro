f13ecac81c22186e52f47f51f7301fa60ad3ef6f
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-09 18:28:48 -0600

    refactor: Update Login component to use Keycloak for authentication

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,8 +1,16 @@
-// src/components/Login.tsx
-"use client"
+"use client";
 import { signIn } from "next-auth/react";
+import { Button } from "@/components/ui/button";
+
 export default function Login() {
-  return <button onClick={() => signIn("keycloak")}>
-    Signin with keycloak
-  </button>
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <Button 
+        onClick={() => signIn("keycloak")} 
+        className="px-6 py-2 bg-primary text-black rounded-md shadow-md hover:bg-primary/90"
+      >
+        Iniciar Sesión Keycloak
+      </Button>
+    </div>
+  );
 }
