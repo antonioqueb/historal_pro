944320c738b3205097a57266fd4a484c26a8514a
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-09 21:50:26 -0600

    Up

---
+++ app/api/auth/signin/page.js
@@ -0,0 +1,25 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Login from "@/components/Login";
+import { getServerSession } from "next-auth";
+import { redirect, useParams } from "next/navigation";
+
+const signinErrors = {
+  // ...
+  "default": ""
+};
+
+export default async function Signin({ searchParams }) {
+  const { callbackUrl, error } = searchParams;
+  const session = await getServerSession(authOptions);
+  if (session) {
+    redirect(callbackUrl || "/");
+  }
+  return (
+    <div>
+      {error && <div>
+        {signinErrors[error.toLowerCase()]}
+      </div>}
+      <Login />
+    </div>
+  );
+}
