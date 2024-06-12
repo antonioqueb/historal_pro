a1bcbebd36babe78017cb02df993e3da73b538dc
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-12 14:54:53 -0600

    refactor: Update next.config.mjs to include webpack alias for project root directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
-// next.config.mjs
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-    output: 'standalone',  // Asegúrate de agregar esta línea
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, './');
+    return config;
+  },
+};
+
+export default nextConfig;
+npm
